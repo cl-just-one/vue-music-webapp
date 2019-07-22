9c7ea331ce4430ebcb1c57aee8a3d92559809287
chenglin <837460387@qq.com>
2019-07-22 15:50:13 +0800

    initialize project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped lang="stylus">
+  @import "~common/stylus/variable";
+
+  #app
+    background: $color-theme
+</style>
